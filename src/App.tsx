30504126d160c93ea0a183ec75968b13ed72be5f
Vitalii <shut.vitaliy.ol@gmail.com>
2024-07-25 18:42:56 +0300

    create grid adopted  layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
 import React from 'react';
+import { Grid } from './components/Grid/Grid.tsx';
 
-export const App: React.FC = () => <h1>Hello World!</h1>;
+export const App: React.FC = () => <Grid />;
---
+++ src/shared/ui/Select/Select.tsx
@@ -0,0 +1,53 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable react/react-in-jsx-scope */
+import React from 'react';
+import Select, { StylesConfig } from 'react-select';
+
+interface Props {
+  option: object[];
+}
+
+const customStyles: StylesConfig = {
+  control: (provided, state) => ({
+    ...provided,
+    backgroundColor: state.isFocused ? '#FAFBFC' : '#fff',
+    boxShadow: 'none',
+    borderColor: '#B4BDC3',
+    fontFamily: 'Mont, sans-serif',
+    fontSize: '14px',
+    outline: 'none',
+    width: '136px',
+    hight: '40px',
+    '&:hover': {
+      borderColor: '#89939A',
+    },
+    '&:focus': {
+      borderColor: '#313237',
+      backgroundColor: '#313237',
+    },
+  }),
+  option: (provided) => ({
+    ...provided,
+    backgroundColor: '#fff',
+    color: '#89939A',
+    fontFamily: 'Mont, sans-serif',
+    fontSize: '14px',
+    fontWeight: '500',
+    '&:hover': {
+      backgroundColor: '#f8f9fa',
+      color: '#313237',
+    },
+  }),
+  indicatorSeparator: (provided) => ({
+    ...provided,
+    display: 'none',
+  }),
+};
+
+export const SelectComponent: React.FC<Props> = ({ option }) => (
+  <Select
+    styles={customStyles}
+    options={option}
+    defaultValue={option[0]}
+  />
+);
