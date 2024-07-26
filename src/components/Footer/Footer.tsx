d4dbe52905d197690aafbf419553fb55a1e545df
Stanislav <stanislavandr@gmail.com>
2024-07-26 13:56:50 +0300

    add adaptive footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,11 +4,14 @@ import './Footer.scss';
 
 export const Footer: React.FC = () => (
   <footer className="footer">
-    <NavLink to="/" className="footer__logo__link">
+    <a href="/home" className="footer__logo-link">
       <img src="/img/logo.png" alt="logo" className="footer__logo" />
-    </NavLink>
-    <div className="footer__container">
-      <NavLink className="footer__link footer__item" to="https://github.com/fe-apr24-team2">
+    </a>
+    <div className="footer__links">
+      <NavLink
+        className="footer__link footer__item"
+        to="https://github.com/fe-apr24-team2"
+      >
         Github
       </NavLink>
       <NavLink className="footer__link footer__item" to="/">
@@ -19,7 +22,7 @@ export const Footer: React.FC = () => (
       </NavLink>
     </div>
     <div className="back-to-top">
-      <p className="footer__item">Back to top</p>
+      <span className="footer__item back-to-top__text">Back to top</span>
       <a className="back-to-top__link" href="#/">
         <img
           src="/img/slider-button.png"
