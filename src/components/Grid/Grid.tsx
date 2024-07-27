3b23d23103eeb1b10438bc931ea92b2473c8ee5b
Les Dyman <lesdyman@gmail.com>
2024-07-27 11:38:58 +0200

    add sorting by year and pagination

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -1,13 +1,14 @@
+import classNames from 'classnames';
 import { useEffect, useState } from 'react';
 import { SelectComponent } from '../../shared/ui/Select/Select';
 import './Grid.scss';
-import { Device } from '../../types/Device';
-import { getPhones } from '../../utils/api';
+import { getProducts } from '../../utils/api';
 import { ProductCard } from '../ProductCard/ProductCard';
+import { Product } from '../../types/Product';
 
 const SortOptions = [
   { value: 'newest', label: 'Newest' },
-  { value: 'latest', label: 'Latest' },
+  { value: 'oldest', label: 'Oldest' },
 ];
 
 const onPageCountOptions = [
@@ -17,36 +18,61 @@ const onPageCountOptions = [
 ];
 
 export const Grid = () => {
-  const [allPhones, setAllPhones] = useState<Device[]>([]);
-  const [displayedPhones, setDisplayedPhones] = useState<Device[]>([]);
+  const [allPhones, setAllPhones] = useState<Product[]>([]);
+  const [displayedPhones, setDisplayedPhones] = useState<Product[]>([]);
   const [itemsPerPage, setItemsPerPage] = useState<number>(10);
+  const [currentPage, setCurrentPage] = useState<number>(1);
 
-  const fetchPhones = async () => {
+  const loadPhones = async () => {
     try {
-      const phonesData = await getPhones();
-      setAllPhones(phonesData);
-      setDisplayedPhones(phonesData.slice(0, itemsPerPage));
+      const data = await getProducts();
+      const onlyPhones = data.filter((item) => item.category === 'phones');
+      setAllPhones(onlyPhones);
     } catch (error) {
       throw new Error(`Error has occurred: ${error}`);
     }
   };
 
   useEffect(() => {
-    fetchPhones();
-  });
+    loadPhones();
+  }, []);
 
   useEffect(() => {
-    setDisplayedPhones(allPhones.slice(0, itemsPerPage));
-  }, [itemsPerPage, allPhones]);
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    setDisplayedPhones(allPhones.slice(startIndex, endIndex));
+  }, [itemsPerPage, currentPage, allPhones]);
 
   const handleItemsPerPageChange = (selectedOption: { value: string }) => {
     setItemsPerPage(parseInt(selectedOption.value, 10));
+    setCurrentPage(1);
   };
 
-  const handleSortByVersion = (selectedOption: { value: string }) => {
+  const handleSortByYear = (selectedOption: { value: string }) => {
     const select = selectedOption.value;
-    // eslint-disable-next-line no-console
-    console.log(select);
+    const itemsToSort = [...allPhones];
+    const sorted = itemsToSort.sort((item1, item2) => {
+      if (select === 'newest') {
+        return item2.year - item1.year;
+      }
+      if (select === 'oldest') {
+        return item1.year - item2.year;
+      }
+      return 0;
+    });
+    setAllPhones(sorted);
+  };
+
+  const getTotalPages = (
+    devices: Product[],
+    devicesPerPage: number,
+  ): number[] => {
+    const pageCount = Math.ceil(devices.length / devicesPerPage);
+    return Array.from({ length: pageCount }, (_, i) => i + 1);
+  };
+
+  const handlePageChange = (pageNumber: number) => {
+    setCurrentPage(pageNumber);
   };
 
   return (
@@ -75,7 +101,7 @@ export const Grid = () => {
             <p className="list-params__sort-title">Sort by</p>
             <SelectComponent
               option={SortOptions}
-              handleChange={handleSortByVersion}
+              handleChange={handleSortByYear}
             />
           </span>
           <span className="list-params__items-on-page">
@@ -89,7 +115,7 @@ export const Grid = () => {
         <div className="component__wrap">
           <div className="component__list list">
             {displayedPhones.map((phone) => (
-              <ProductCard phone={phone} />
+              <ProductCard phone={phone} key={phone.id} />
             ))}
           </div>
         </div>
@@ -98,23 +124,29 @@ export const Grid = () => {
             className="nav-wrap__arrow-back"
             type="button"
             aria-label="back"
+            disabled={currentPage === 1}
+            onClick={() => handlePageChange(currentPage - 1)}
           />
-          <button className="nav-wrap__page" type="button">
-            1
-          </button>
-          <button className="nav-wrap__page" type="button">
-            2
-          </button>
-          <button className="nav-wrap__page" type="button">
-            3
-          </button>
-          <button className="nav-wrap__page" type="button">
-            4
-          </button>
+          {getTotalPages(allPhones, itemsPerPage).map((page) => (
+            <button
+              className={classNames('nav-wrap__page', {
+                active: page === currentPage,
+              })}
+              type="button"
+              key={page}
+              onClick={() => handlePageChange(page)}
+            >
+              {page}
+            </button>
+          ))}
           <button
             className="nav-wrap__arrow-next"
             type="button"
             aria-label="next"
+            disabled={
+              currentPage === getTotalPages(allPhones, itemsPerPage).length
+            }
+            onClick={() => handlePageChange(currentPage + 1)}
           />
         </div>
       </div>
