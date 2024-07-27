839ab1be0bcbe0ad0f5cce61eae9aaf3da93880c
Natulik s <aakkrryyttoo99@gmail.com>
2024-07-27 17:06:28 +0300

    add header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,42 +1,77 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
+/* eslint-disable jsx-a11y/anchor-has-content */
+import { NavLink, HashRouter as Router } from 'react-router-dom';
+import './index.scss';
 import React from 'react';
-import { NavLink } from 'react-router-dom';
-import './Header.scss';
 
-export const Header: React.FC = () => (
-  <nav>
-    <ul>
-      <li>
-        <NavLink
-          to="/"
-          className={({ isActive }) => `nav__link ${isActive ? 'is-active' : ''}`}
-        >
-          Home
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          to="/phones"
-          className={({ isActive }) => `nav__link ${isActive ? 'is-active' : ''}`}
-        >
-          Phones
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          to="/tablets"
-          className={({ isActive }) => `nav__link ${isActive ? 'is-active' : ''}`}
-        >
-          Tablets
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          to="/accessories"
-          className={({ isActive }) => `nav__link ${isActive ? 'is-active' : ''}`}
-        >
-          Accessories
-        </NavLink>
-      </li>
-    </ul>
-  </nav>
-);
+export const Header: React.FC = () => {
+  return (
+    <header className="header">
+      <div className="container">
+        <div className="header__left">
+          <a href="home" className="header__logo">
+            <img src="./src/page/header/logo.svg" alt="Nice Gadgets logo" />
+          </a>
+
+          <Router>
+            <nav>
+              <ul className="nav__list">
+                <li className="nav__item">
+                  <NavLink
+                    to="/"
+                    className={({ isActive }) => `nav__link ${isActive ? 'is-active' : ''}`}
+                  >
+                    Home
+                  </NavLink>
+                </li>
+                <li className="nav__item">
+                  <NavLink
+                    to="/phones"
+                    className={({ isActive }) => `nav__link ${isActive ? 'is-active' : ''}`}
+                  >
+                    Phones
+                  </NavLink>
+                </li>
+                <li className="nav__item">
+                  <NavLink
+                    to="/tablets"
+                    className={({ isActive }) => `nav__link ${isActive ? 'is-active' : ''}`}
+                  >
+                    Tablets
+                  </NavLink>
+                </li>
+                <li className="nav__item">
+                  <NavLink
+                    to="/accessories"
+                    className={({ isActive }) => `nav__link ${isActive ? 'is-active' : ''}`}
+                  >
+                    Accessories
+                  </NavLink>
+                </li>
+              </ul>
+            </nav>
+            {/* <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/phones" element={<PhonesPage />} />
+                <Route path="/tablets" element={<TabletsPage />} />
+                <Route path="/accessories" element={<AccessoriesPage />} />
+                <Route path="*" element={<PageNotFound />} />
+              </Routes> */}
+          </Router>
+        </div>
+
+        <div className="header__right">
+          <div className="icons">
+            <a href="heart" className="icon icon--heart" />
+
+            <a href="cart" className="icon icon--cart" />
+          </div>
+        </div>
+
+        <div className="burger-menu">
+          <img src="./src/page/header/menu.svg" alt="Menu" />
+        </div>
+      </div>
+    </header>
+  );
+};
