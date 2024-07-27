77eeef5358aafb142afe3aa21acaa68866e1af20
Veronika Demko <demkoveronika06@gmail.com>
2024-07-27 15:21:12 +0200

    add one more time

--- src/components/CartItem/CartItem.tsx
+++ src/components/CartItem/CartItem.tsx
@@ -23,22 +23,50 @@ export const CartItem: React.FC = () => {
   return (
     <div className="item">
       <div className="item_desc">
-        <img className="item_image__cross" src="/img/icons/cross.svg" alt="gray cross" />
+        <img
+          className="item_image__cross"
+          src="/img/icons/cross.svg"
+          alt="gray cross"
+        />
 
-        <img src={product.images} alt={product.name} className="item_image__phone" />
+        <img
+          src={product.images}
+          alt={product.name}
+          className="item_image__phone"
+        />
 
         <p className="item_name">{product.name}</p>
       </div>
 
       <div className="item_counter">
         <div className="item_buttons">
-          <button type="button" className="item_button item_button__subtract" disabled={isMinusDisabled}>
-            <img className="item_button__image-black" src="/img/icons/minusBlack.svg" alt="subtract" />
-            <img className="item_button__image-gray" src="/img/icons/minusGray.svg" alt="disabled minus" />
+          <button
+            type="button"
+            className="item_button item_button__subtract"
+            disabled={isMinusDisabled}
+          >
+            <img
+              className="item_button__image-black"
+              src="/img/icons/minusBlack.svg"
+              alt="subtract"
+            />
+            <img
+              className="item_button__image-gray"
+              src="/img/icons/minusGray.svg"
+              alt="disabled minus"
+            />
           </button>
           <p className="item_count">1</p>
-          <button type="button" className="item_button item_button__add" onClick={handlePlusClick}>
-            <img className="item_button__image" src="/img/icons/plus.svg" alt="add" />
+          <button
+            type="button"
+            className="item_button item_button__add"
+            onClick={handlePlusClick}
+          >
+            <img
+              className="item_button__image"
+              src="/img/icons/plus.svg"
+              alt="add"
+            />
           </button>
         </div>
 
